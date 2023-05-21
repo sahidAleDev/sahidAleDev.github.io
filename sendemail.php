7b35d29f54deaa370da8e0c7c2435f5fd0085fd8
sahidAle <sahidalejandroo@gmail.com>
2023-05-20 20:55:59 -0600

    Arreglados pequeños detalles
    Full arreglos

---
+++ sendemail.php
@@ -0,0 +1,33 @@
+<?php
+
+// Define some constants
+define( "RECIPIENT_NAME", "sahid" );
+define( "RECIPIENT_EMAIL", "sahidaledev@gmail.com" );
+
+
+// Read the form values
+$success = false;
+$userName = isset( $_POST['username'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['username'] ) : "";
+$lastName = isset( $_POST['lastname'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['lastname'] ) : "";
+$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['email'] ) : "";
+$userPhone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
+$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Message:|Content-Type:)/", "", $_POST['message'] ) : "";
+
+// If all values exist, send the email
+if ( $userName && $lastName && $senderEmail && $userPhone && $message) {
+  echo $msgBody;
+  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
+  $headers = "From: " . $username . "";
+  $msgBody = " Last Name: " . $userName . " Email: " . $senderEmail . " Phone: " . $userPhone . " Message: " . $message . "";
+  $success = mail( $recipient, $headers, $msgBody );
+
+  //Set Location After Successsfull Submission
+  header('Location: index.html?message=Successfull');
+}
+
+else{
+	//Set Location After Unsuccesssfull Submission
+  	header('Location: index.html?message=Failed');	
+}
+
+?>
